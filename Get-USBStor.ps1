32fb60bfb9cb3401966e24bb4344f46d2bfd0249
c0moshack <c0moshack@gmail.com>
2017-02-08 13:34:14 -0600

    Get-USBStor
    Working with updating file ACEs

--- Get-USBStor.ps1
+++ Get-USBStor.ps1
@@ -29,17 +29,109 @@
 
 	Param(
 		[Parameter(
-		Mandatory=$true,
-		Position=0,
-		ValueFromPipeline=$true,
-		ValueFromPipelineByPropertyName=$true)]
-		#[array]$<VARIABLE>
+		Mandatory=$false)]
+		[switch]$Enable,
+        [Parameter(
+		Mandatory=$false)]
+		[switch]$Disable
+        
 	)
 
-    begin {}
+    begin {
+        #Get the current setting for WriteProtect in the registry
+        Write-Verbose "Getting write protect status"
+        If (Get-Item HKLM:\SYSTEM\CurrentControlSet\Control\StorageDevicePolicies\WriteProtect -ErrorAction SilentlyContinue) {
+            $WriteProtect = $(Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Control\StorageDevicePolicies).WriteProtect
+        } Else {
+            $WriteProtect = "DOES NOT EXIST"
+        }
+        Write-Verbose "WriteProtect Status: $WriteProtect"
 
-    process {}
+        #Get details about "C:\Windows\System32\drivers\USBSTOR.SYS"
+        $SYSPath = "C:\Windows\System32\drivers\USBSTOR.SYS"
+        $SYSACL = Get-Acl $SYSPath
+        $SYSOwner = (Get-Acl $SYSPath).Owner
+        Write-Verbose "USBSTOR.SYS Owner: $SYSOwner"
+        $SYSAccess = (Get-Acl $SYSPath).Access | Select-Object IdentityReference, FileSystemRights
+        Write-Verbose "USBSTOR.SYS Access: $($SYSAccess | Out-String)"
+        
+        #Get details about "C:\Windows\INF\usbstor.PNF"
+        $PNFPath = "C:\Windows\INF\usbstor.PNF"
+        $PNFACL = Get-Acl $PNFPath
+        $PNFOwner = (Get-Acl $PNFPath).Owner
+        Write-Verbose "usbstor.PNF Owner: $PNFOwner"
+        $PNFAccess = (Get-Acl $PNFPath).Access | Select-Object IdentityReference, FileSystemRights
+        Write-Verbose "usbstor.PNF Access: $($PNFAccess | Out-String)"
+        
+        #Get details about "C:\Windows\INF\usbstor.inf"
+        $INFPath = "C:\Windows\INF\usbstor.inf"
+        $INFACL = Get-Acl $INFPath
+        $INFOwner = (Get-Acl $INFPath).Owner
+        Write-Verbose "usbstor.inf Owner: $INFOwner"
+        $INFAccess = (Get-Acl $INFPath).Access | Select-Object IdentityReference, FileSystemRights
+        Write-Verbose "usbstor.inf Access: $($INFAccess | Out-String)"
+    }
+
+    process {
+        #Check if we even need to do anything first!
+        If ( ($WriteProtect = "DOES NOT EXIST") -or ($WriteProtect = "0") -and (($INFAccess | Where-Object {$_.IdentityReference -like "BUILTIN\Users"}).FileSystemRights -like "ReadAndExecute*") -eq $true) {
+            Write-Output "USB Storage is already enabled and writeable."
+        } ElseIf  ( ($WriteProtect = "1") -and (($INFAccess | Where-Object {$_.IdentityReference -like "BUILTIN\Users"}).FileSystemRights -like "ReadAndExecute*") -eq $true) {
+            Write-Output "USB Storage is already enabled but not writeable."
+            $enableWrite = $true
+        } Else {
+            Write-Output "USB Storage appears to be disabled"
+            $enableWrite = $true
+            $enableUSB = $true
+
+            If ($Enable) {
+                If ($enableWrite) {
+                    New-Item HKLM:\SYSTEM\CurrentControlSet\Control\StorageDevicePolicies -ErrorAction SilentlyContinue
+                    Set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Control\StorageDevicePolicies -Name "WriteProtect" -Value "0"
+                }
+                If ($enableUSB) {
+                    #Take owner ship of the appropriate files in order to change security settings
+                    & TAKEOWN /F $SYSPath
+                    & TAKEOWN /F $PNFPath
+                    & TAKEOWN /F $INFPath
+
+                    # Grant Users rights to the files (Everyone should be a user even if they have admin rights)
+                    $newACL = $SYSACL
+                    $newPermissions = $env:username, 'Read,Modify', 'Allow'
+                    $newRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $newPermissions
+                    $newACL.SetAccessRule($newRule)
+                    Set-Acl -Path $SYSPath -AclObject $newACL
+
+                    $newACL = $PNFACL
+                    $newPermissions = $env:username, 'Read,Modify', 'Allow'
+                    $newRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $newPermissions
+                    $newACL.SetAccessRule($newRule)
+                    Set-Acl -Path $PNFPath -AclObject $newACL
+
+                    $newACL = $INFACL
+                    $newPermissions = $env:username, 'Read,Modify', 'Allow'
+                    $newRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $newPermissions
+                    $newACL.SetAccessRule($newRule)
+                    Set-Acl -Path $INFPath -AclObject $newACL
+                }
+            }
+
+            If ($Disable) {
+                # Restore the original file permissions
+                Set-Acl -Path $SYSPath -AclObject $SYSACL   
+                Set-Acl -Path $PNFPath -AclObject $PNFACL   
+                Set-Acl -Path $INFPath -AclObject $INFACL   
+            }
+        }
+        
+    }
 	
-    end {}
+    end {
+        
+        
+
+    }
 
 }
+
+Get-USBStor -Enable
---
+++ Project_PDFConverter/Project_SCCM/sccmqueries.ps1
@@ -0,0 +1,129 @@
+﻿Function Get-SCCMDeployment {
+	[cmdletbinding()]
+	Param(
+		[Parameter(
+		Mandatory=$false,
+		Position=0)]
+		[string]$SiteServer,
+		
+		[Parameter(
+		Mandatory=$false,
+		Position=1)]
+		[string]$SiteCode,
+				
+		[Parameter(
+		Mandatory=$false,
+		Position=2)]
+		[string]$State
+	)
+
+	$deployments = get-wmiobject -ComputerName $SiteServer -NameSpace "ROOT\SMS\site_$SiteCode" -Class SMS_DeploymentInfo
+	$statedeployments = $deployments | where {$_.CollectionName -match "^$State\s?-\s.+"}  
+	
+	return $statedeployments
+}
+
+Function Get-SCCMDeploymentInfo {
+	[cmdletbinding()]
+	Param(
+		[Parameter(
+		Mandatory=$false,
+		Position=0)]
+		[array]$Deployment
+	)
+	
+	$deploymentdata = @()
+	
+	Foreach ($one in $Deployment) {
+		#Invoke-CMDeploymentSummarization -DeploymentId $($one.DeploymentID) -ErrorAction SilentlyContinue
+		$deploymentinfo = Get-CMDeployment -DeploymentId $($one.DeploymentID)
+		
+		$temp = @{}
+		$temp.Collection = $($deploymentinfo.CollectionName)
+		$temp.Deployment = $($deploymentinfo.SoftwareName)
+		$temp.DeploymentID = $($deploymentinfo.DeploymentID)
+		$temp.Started = $($deploymentinfo.DeploymentTime)
+		$temp.Total = $($deploymentinfo.NumberTargeted)
+		$temp.Complete =  $($deploymentinfo.NumberSuccess)
+		$temp.InProgress = $($deploymentinfo.NumberInProgress)
+		$temp.Error = $($deploymentinfo.NumberErrors)
+		$temp.Unknown = $($($deploymentinfo.NumberOther) + $($deploymentinfo.NumberUnknown))
+		
+		$deploymentobject = New-Object -TypeName PSObject -Property $temp
+		
+		$deploymentdata += $deploymentobject
+	}
+	return $deploymentdata
+}
+
+Function Get-SCCMCollection {
+	[cmdletbinding()]
+	Param(
+		[Parameter(
+		Mandatory=$false,
+		Position=0,
+		ValueFromPipeline=$true,
+		ValueFromPipelineByPropertyName=$true)]
+		[string]$SiteServer,
+		
+		[Parameter(
+		Mandatory=$false,
+		Position=1,
+		ValueFromPipeline=$true,
+		ValueFromPipelineByPropertyName=$true)]
+		[string]$SiteCode,
+				
+		[Parameter(
+		Mandatory=$false,
+		Position=2,
+		ValueFromPipeline=$true,
+		ValueFromPipelineByPropertyName=$true)]
+		[string]$State
+		
+	)
+
+	$Collection = get-wmiobject -ComputerName $siteServer -NameSpace "ROOT\SMS\site_$SiteCode" -Class SMS_Collection 
+	$statecollections = $Collection | where {$_.Name -match "^$State\s-\s.+"}  
+	
+	$collectiondata = @()
+	
+	Foreach ($item in $statecollections) {
+	
+		$temp = @{}
+		$temp.CollectionID = $($item.CollectionID)
+		$temp.Name = $($item.Name)
+		$temp.Comment = $($item.Comment)
+		$temp.LocalMemberCount = $($item.LocalMemberCount)
+		$temp.MemberCount = $($item.MemberCount)
+		$temp.LimitToCollectionID = $($item.LimitToCollectionID)
+		$temp.LimitToCollectionName = $($item.LimitToCollectionName)
+		
+		$collectionobject = New-Object -TypeName PSObject -Property $temp
+		
+		$collectiondata += $collectionobject
+	
+	}
+	
+	return $collectiondata
+	
+}
+
+$starttime = $(Get-Date)
+
+$params = @{
+	SiteServer = 'NGEWA0-SCCM02.ng.ds.army.mil'
+	SiteCode = 'NG4'
+	State = 'WI'
+}	
+
+$collections = Get-SCCMCollection @params | Select Name,CollectionID,LocalMemeberCount,MemberCount,Comment #| ConvertTo-Html | Out-File C:\Users\paul.j.brown\Desktop\SCCMReport.html
+$deploymentstatus = Get-SCCMDeploymentInfo -Deployment $(Get-SCCMDeployment @params) | Select Deployment,DeploymentID,Started,Total,InProgress,Complete,Error,Unknown #| ConvertTo-Html | Out-File C:\Users\paul.j.brown\Desktop\SCCMReport.html -Append
+
+$endtime = $(Get-Date)
+
+$head = '<style>BODY{background-color:#DDDDDD;}TABLE{border-width:1px;border-style:solid;border-color:black;border-collapse:collapse;}TH{border-width: 1px;padding: 2px;border-style: solid;border-color: black;}TD{border-width: 1px;padding: 2px;border-style: solid;border-color: black;}</style>'
+$title = 'SCCM Status Report'
+$body = "Report Start: $starttime <br /> Report End: $endtime <br /> Report Generated: $(Get-Date) <br /> <h2>Device Collections</h2>$($collections | ConvertTo-Html -Fragment) <br /> <h2>Deployments</h2>$($deploymentstatus | ConvertTo-Html -Fragment)"
+$reporttime = Get-Date -Format 'yyMMdd-HHmm'
+ConvertTo-Html -Head $head -Title $title -Body $body | Out-File C:\Users\paul.j.brown\Desktop\SCCMReports\SCCMReport_$($reporttime).html
+Export-PDF -Path "C:\Users\paul.j.brown\Desktop\SCCMReports\*.*" -Extension "*.html"
