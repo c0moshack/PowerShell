b79f3553a2869cd7e765df5d4a9c1f0b39096efc
Brown <1244925690@mil>
2018-02-16 11:46:34 -0600

    git update

---
+++ Get-JSONInventory.ps1
@@ -0,0 +1,15 @@
+﻿$subOperatingSystem = @()
+$subOperatingSystem += [pscustomobject]@{
+    'name' = $env:COMPUTERNAME;
+    'domain' = $env:USERDOMAIN;
+    'architecture' = $env:PROCESSOR_ARCHITECTURE
+    'serial' = (Get-WmiObject -Class Win32_Bios).SerialNumber
+}
+
+
+
+$systems = [pscustomobject]@{
+    'systems' = $subOperatingSystem
+}
+
+$systems | ConvertTo-Json
--- WindowsEVTtoJSON/Process-WinEventLogs.ps1
+++ WindowsEVTtoJSON/Process-WinEventLogs.ps1
@@ -1,5 +1,5 @@
 ﻿# ----------------------------------------------------------------------------- 
-# Script: Untitled3.ps1
+# Script: Process-WinEventLogs.ps1
 # Author: Paul Brown
 # Date: 12/13/2017 08:43:57 
 # Keywords: 
@@ -7,7 +7,7 @@
 # 
 # -----------------------------------------------------------------------------
 
- Function <NAME> {
+ Function Process-WinEventLogs {
 	<# 
 	    .Synopsis 
 	   		This does that  
@@ -34,7 +34,7 @@
 		ValueFromPipeline=$true,
 		ValueFromPipelineByPropertyName=$true)]
 		#[string]$WorkDirectory = $(Split-Path $MyInvocation.MyCommand.Path)
-        [string]$WorkDirectory = "C:\Users\paul.j.brown\Desktop\WindowsEVTtoJSON"
+        [string]$WorkDirectory #= "C:\Users\paul.j.brown\Documents\Git\PowerShell\WindowsEVTtoJSON"
 	)
     begin {
         # Set the log name format
@@ -81,8 +81,77 @@
     
 
     end {
-        
+        Write-Host "$(Get-ChildItem $OutputDirectory).length files created."
     }
     
 
 }
+
+Process-WinEventLogs -WorkDirectory "C:\Users\paul.j.brown\Documents\Git\PowerShell\WindowsEVTtoJSON"
+
+# ----------------------------------------------------------------------------- 
+# Script: Untitled3.ps1
+# Author: Paul Brown
+# Date: 12/14/2017 10:18:31 
+# Keywords: 
+# comments: 
+# 
+# -----------------------------------------------------------------------------
+
+ Function Import-LogFiles {
+	<# 
+	    .Synopsis 
+	   		This does that  
+	   	.Example 
+	    	Example- 
+	    .Parameter  
+	    	The parameter 
+	    .Notes 
+	    	NAME: Untitled3.ps1 
+	    	AUTHOR: paul.j.brown 
+	    	LASTEDIT: 12/14/2017 10:18:31 
+	    	KEYWORDS: 
+	    .Link 
+	    	https://gallery.technet.microsoft.com/scriptcenter/site/search?f%5B0%5D.Type=User&f%5B0%5D.Value=PaulBrown4 
+	#Requires -Version 2.0 
+	#>
+	[CmdletBinding()]
+    [OutputType([psobject])]
+    
+	Param(
+		[Parameter(
+		Mandatory=$true,
+		Position=0)]
+		[string]$SourceDirectory,
+
+        [Parameter(
+        Mandatory=$true,
+		Position=1)]
+        [string]$DestinationDirectory,
+
+        [Parameter(
+        Mandatory=$false,
+		Position=1)]
+        [int]$Days = 1
+	)
+    begin {
+        $files = Get-Childitem $SourceDirectory -Recurse -Includ @("*.evt","*.evtx") | Where-Object {$_.CreationTime -gt (Get-Date).AddDays("-$Days")}
+    }
+	
+
+    process {
+        Foreach ($file in $files) {
+            Copy-Item $file $DestinationDirectory
+            #Write-Host "Copied $file TO $DestinationDirectory"
+        }
+    }
+    
+
+    end {
+        Write-Host "Copied $($files.Length) files"
+    }
+    
+
+}
+
+#mport-LogFiles -SourceDirectory "\\ngwiem-toolsvr\domain event logs\Domain Controllers" -DestinationDirectory "C:\Users\paul.j.brown\Documents\Git\PowerShell\WindowsEVTtoJSON\Dropbox"
